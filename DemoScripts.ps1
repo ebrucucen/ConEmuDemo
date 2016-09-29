fea75b952ac5a970fe00975ca43e2acfccf6ee69
Ebru Cucen <ebrugul@hotmail.com>
2016-09-29 14:57:51 +0100

    Added chocolate param, rearranged order

--- DemoScripts.ps1
+++ DemoScripts.ps1
@@ -1,22 +1,35 @@
 ﻿################################
 ##Step1: Choco, ConEmu, Posh-Git
 ################################
- 
-# Set your PowerShell execution policy
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
+#Check if you are running as Admin: 
+function Test-Administrator {
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
 
-# Install Chocolatey
-iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+if (Test-Administrator){
+    # Set your PowerShell execution policy
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
 
-# Install Chocolatey packages
-choco install git.install -y
-choco install conemu -y
+    # Install Chocolatey
+    Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 
-# Install PowerShell modules
-Install-PackageProvider NuGet -MinimumVersion '2.8.5.201' -Force
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Module -Name 'posh-git'
+    #Set Autoconfirmation for installs
+    choco feature enable -name=allowGlobalConfirmation
+    
+    # Install Chocolatey packages
+    choco install git.install -y
+    choco install conemu -y
 
+    # Install PowerShell modules
+    Install-PackageProvider NuGet -MinimumVersion '2.8.5.201' -Force
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    Install-Module -Name 'posh-git'
+}
+else
+{
+    write-output "You need to Run as Administrator"
+}
 ################################
 Step2: ConEmu Features
 ################################
@@ -28,36 +41,7 @@ Step2: ConEmu Features
 #ANSI Escape Character
 
 ################################
-Step3: Profile Settings 
-################################
-
-# Creates profile if doesn't exist then edits it
-if (!(Test-Path -Path $PROFILE)){ New-Item -Path $PROFILE -ItemType File } ; ise $PROFILE
-
-#----
-function prompt {
-Write-Host
-$E=[char]0x001b; 
-Write-Host "$E`[1;33;40m $ENV:USERNAME@ $E`[0m" -NoNewline
-if(Test-Administrator) {
-write-host "$E[91;107m(Admin) $E`[0m" -NoNewline  }
-Write-Host "$E`[97;102m$ENV:COMPUTERNAME$E`[0m" -NoNewline
-write-vcsStatus
-Write-Host ""
-Return "> "
-
-}
-function Test-Administrator {
-    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
-    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-}
-#----
-Import-Module -Name posh-git
-
-Start-SshAgent
-
-################################
-Step4: Git Setup
+Step3: Git Setup
 ################################
 
 #----
@@ -95,3 +79,44 @@ git config --global core.autocrlf true
 #---
 #Navigate to the lication and git clone: 
 New-Item -Path "C:\" -ItemType "Directory" -Name "Projects" -Force
+
+######
+################################
+Step3: Profile Settings 
+################################
+
+# Creates profile if doesn't exist then edits it
+if (!(Test-Path -Path $PROFILE))
+{ 
+    New-Item -Path $PROFILE -ItemType File 
+} 
+ise $PROFILE
+
+#get the referenced libraries: 
+
+Set-Location -Path "c:/projects"
+git clone "https://github.com/joonro/Get-ChildItem-Color.git" Get-ChildItem-Color
+
+#start of the profile file:
+#---- These will be the basic of the profile
+function Test-Administrator {
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+function prompt {
+    Write-Host
+    $E=[char]0x001b; 
+    Write-Host "$E`[1;33;40m $ENV:USERNAME@ $E`[0m" -NoNewline
+    if(Test-Administrator) {
+        write-host "$E[91;107m(Admin) $E`[0m" -NoNewline  
+    }
+    Write-Host "$E`[97;102m$ENV:COMPUTERNAME$E`[0m" -NoNewline
+    Write-vcsStatus
+    Write-Host ""
+    Return "> "
+}
+#----
+Import-Module -Name posh-git
+Stop-SshAgent
+Start-SshAgent
+#end of profile file
